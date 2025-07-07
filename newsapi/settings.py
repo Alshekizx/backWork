4428bf0717fc8ea71407589dfd9a30c4144554df
seyi <seyiduncan40@gmail.com>
2025-07-07 13:31:13 +0100

    jd-database url

--- newsapi/settings.py
+++ newsapi/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,15 +84,10 @@ WSGI_APPLICATION = 'newsapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'news_db',
-        'USER': 'seyi',
-        'PASSWORD': 'Adeyemi40Duncan!',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 AUTH_USER_MODEL = 'news.CustomUser'
