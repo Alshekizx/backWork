46f25f05b8e7aeaa01a42ed3d4580ef208d7fcc5
Your Name <you@example.com>
2025-12-02 17:21:58 +0100

    add url /home

--- newsapi/urls.py
+++ newsapi/urls.py
@@ -3,6 +3,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from news.view2.custom_auth import CustomTokenLoginView
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, NaijaTalk API is running!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
